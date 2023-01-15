352a3d01d7e242ac86f7d80c41bedb14f9332f6b
madedeva <deva.kerti@undiksha.ac.id>
2023-01-15 19:22:04 +0800

    konfigurasi API

---
+++ lib/config/api.dart
@@ -0,0 +1,11 @@
+class Api {
+  static const String _baseUrl = "http://10.10.5.115:8080/discuss_app_api";
+  static const comment = "$_baseUrl/comment";
+  static const follow = "$_baseUrl/follow";
+  static const topic = "$_baseUrl/topic";
+  static const user = "$_baseUrl/user";
+
+  static const imageComment = "$_baseUrl/image/comment";
+  static const imageTopic = "$_baseUrl/image/topic";
+  static const imageUser = "$_baseUrl/image/user";
+}
